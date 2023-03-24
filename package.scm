a71cac549a8b22d3f5e1c1bd5c7010723c9837e6
Shiro Kawai <shiro@acm.org>
2023-03-23 23:56:34 -1000

    Replace configure script, add package.scm
    Modern Gauche no longer needs autoconf, and package.scm is
    useful for package metainformaton.

---
+++ package.scm
@@ -0,0 +1,45 @@
+;;
+;; Package Gauche-memcache
+;;
+
+(define-gauche-package "Gauche-memcache"
+  ;; Repository URL, e.g. github
+  ;;  This URL uniquely identifies the package.
+  :repository "https://github.com/tabe/Gauche-memcache.git"
+
+  ;;
+  :version "0.1"
+
+  ;; Description of the package.  The first line is used as a short
+  ;; summary.
+  :description "Memcached client for Gauche"
+
+  ;; List of dependencies.
+  ;; Example:
+  ;;     :require (("Gauche" (>= "0.9.5"))  ; requires Gauche 0.9.5 or later
+  ;;               ("Gauche-gl" "0.6"))     ; and Gauche-gl 0.6
+  :require (("Gauche" (>= "0.9.7")))
+
+  ;; List of providing modules
+  ;; NB: This will be recognized >= Gauche 0.9.7.
+  ;; Example:
+  ;;      :providing-modules (util.algorithm1 util.algorithm1.option)
+  :providing-modules (memcache)
+
+  ;; List name and contact info of authors.
+  ;; e.g. ("Eva Lu Ator <eval@example.com>"
+  ;;       "Alyssa P. Hacker <lisper@example.com>")
+  :authors ("Takeshi Abe <tabe@fixedpoint.jp>")
+
+  ;; List name and contact info of package maintainers, if they differ
+  ;; from authors.
+  ;; e.g. ("Cy D. Fect <c@example.com>")
+  :maintainers ()
+
+  ;; List licenses
+  ;; e.g. ("BSD")
+  :licenses ("BSD")
+
+  ;; Homepage URL, if any.
+  :homepage "https://fixedpoint.jp/gauche-memcache/"
+  )
